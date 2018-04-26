8e7a0cdfbb0e15c0224f868d892dd89bdbe42fc1
g16921km <g16921km@s30445.tsuda.ac.jp>
2018-04-26 14:01:29 +0900

    コミット

---
+++ test/snakecamel/SnakeTest.java
@@ -0,0 +1,14 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
